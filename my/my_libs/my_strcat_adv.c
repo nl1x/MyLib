cdbb712ac632778534ef36904c5318902ceedbf7
L1x (AKA Nathan J.) <nathan.jeannot@epitech.eu>
2023-11-20 12:05:50 +0100

    fix (./my_libs/my_strcat_adv.c): fix git problem

--- my/my_libs/my_strcat_adv.c
+++ my/my_libs/my_strcat_adv.c
@@ -12,19 +12,8 @@ char *my_strcat_adv(char *start, char *end)
 {
     int end_len = my_strlen(end);
 
-<<<<<<< HEAD
     start = my_realloc(start, end_len + 1);
     start = my_strcat(start, end);
     return start;
-=======
-    for (int i = 0; i < start_length; i++) {
-        str[x] = start[i];
-        x++;
-    }
-    for (int i = 0; i < end_length; i++) {
-        str[x] = end[i];
-        x++;
-    }
-    return str;
->>>>>>> refs/remotes/origin/main
 }
+
